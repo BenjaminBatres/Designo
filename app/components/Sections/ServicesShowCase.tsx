baccde419a3b409caf39170027943e9c36a6e9ea
Benjamin Batres <benbatres1212@gmail.com>
2026-09-24 08:53:11 -0700

    feat: implemented showcase section

---
+++ app/components/Sections/ServicesShowCase.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import WebDesignImage from "../../assets/home/desktop/image-web-design-large.jpg";
+import WebDesignImageTablet from "../../assets/home/tablet/image-web-design.jpg";
+import WebDesignImageMobile from "../../assets/home/mobile/image-web-design.jpg";
+import AppDesignImageDesktop from "../../assets/home/desktop/image-app-design.jpg";
+import AppDesignImageTablet from "../../assets/home/tablet/image-app-design.jpg";
+import AppDesignImageMobile from "../../assets/home/mobile/image-app-design.jpg";
+import GraphicDesignImageDesktop from "../../assets/home/desktop/image-graphic-design.jpg";
+import GraphicDesignImageTablet from "../../assets/home/tablet/image-graphic-design.jpg";
+import GraphicDesignImageMobile from "../../assets/home/mobile/image-graphic-design.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+import Link from "next/link";
+export default function ServicesShowCase() {
+  const services = [
+    {
+      imageDesktop: WebDesignImage,
+      imageTablet: WebDesignImageTablet,
+      imageMobile: WebDesignImageMobile,
+      title: "Web Design",
+      path: "/web-design",
+      span: "row-span-2",
+      backgroundColor: "bg-Black/60 lg:bg-Peach/80",
+    },
+    {
+      imageDesktop: AppDesignImageDesktop,
+      imageTablet: AppDesignImageTablet,
+      imageMobile: AppDesignImageMobile,
+      title: "App Design",
+      path: "/app-design",
+      span: "row-span-1",
+      backgroundColor: "bg-Black/60",
+    },
+    {
+      imageDesktop: GraphicDesignImageDesktop,
+      imageTablet: GraphicDesignImageTablet,
+      imageMobile: GraphicDesignImageMobile,
+      title: "Graphic Design",
+      path: "/graphic-design",
+      span: "row-span-1",
+      backgroundColor: "bg-Black/60",
+    },
+  ];
+  return (
+    <section className="pt-15 pb-20 lg:py-20 px-8">
+      <div className="max-w-277.75 mx-auto grid lg:grid-cols-2 gap-x-7.5 gap-y-6">
+        {services.map((service, id) => (
+          <Link
+            href={service.path}
+            key={id}
+            className={`relative rounded-2xl overflow-hidden ${service.span}`}
+          >
+            <Image
+              src={service.imageDesktop}
+              alt=""
+              className="w-full hidden lg:block"
+            />
+            <Image
+              src={service.imageTablet}
+              alt=""
+              className="w-full hidden sm:block lg:hidden"
+            />
+            <Image
+              src={service.imageMobile}
+              alt=""
+              className="w-full sm:hidden"
+            />
+            <div
+              className={`absolute inset-0 ${service.backgroundColor} flex flex-col items-center justify-center gap-6`}
+            >
+              <h2 className="text-[40px]/12 text-white">{service.title}</h2>
+              <div className="flex items-center gap-3">
+                <span className="text-white uppercase text-[15px] tracking-[5px]">
+                  View Project{" "}
+                </span>
+                <MdOutlineKeyboardArrowRight className="text-xl text-Peach" />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import HeroSection from "./components/Sections/HeroSection";
+import ServicesShowCase from "./components/Sections/ServicesShowCase";
 
 export default function Home() {
   return (
     <>
     <HeroSection />
-    
+    <ServicesShowCase />
     </>
   );
 }
