4ff107ab157b4106e017f434dbe1d1a083ab2f95
Benjamin Batres <benbatres1212@gmail.com>
2026-09-24 09:50:12 -0700

    fix: fixed padding on section and changed right arrow icon

--- app/components/Sections/ServicesShowCase.tsx
+++ app/components/Sections/ServicesShowCase.tsx
@@ -8,7 +8,7 @@ import AppDesignImageMobile from "../../assets/home/mobile/image-app-design.jpg"
 import GraphicDesignImageDesktop from "../../assets/home/desktop/image-graphic-design.jpg";
 import GraphicDesignImageTablet from "../../assets/home/tablet/image-graphic-design.jpg";
 import GraphicDesignImageMobile from "../../assets/home/mobile/image-graphic-design.jpg";
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import RightArrow from "../../assets/shared/desktop/icon-right-arrow.svg"
 
 import Link from "next/link";
 export default function ServicesShowCase() {
@@ -42,7 +42,7 @@ export default function ServicesShowCase() {
     },
   ];
   return (
-    <section className="pt-15 pb-20 lg:py-20 px-8">
+    <section className="pt-15 pb-20 lg:py-20 px-7.5">
       <div className="max-w-277.75 mx-auto grid lg:grid-cols-2 gap-x-7.5 gap-y-6">
         {services.map((service, id) => (
           <Link
@@ -53,27 +53,30 @@ export default function ServicesShowCase() {
             <Image
               src={service.imageDesktop}
               alt=""
+              loading="eager"
               className="w-full hidden lg:block"
             />
             <Image
               src={service.imageTablet}
               alt=""
+              loading="eager"
               className="w-full hidden sm:block lg:hidden"
             />
             <Image
               src={service.imageMobile}
               alt=""
+              loading="eager"
               className="w-full sm:hidden"
             />
             <div
-              className={`absolute inset-0 ${service.backgroundColor} flex flex-col items-center justify-center gap-6`}
+              className={`absolute inset-0 ${service.backgroundColor} flex flex-col items-center justify-center gap-3 sm:gap-6`}
             >
-              <h2 className="text-[40px]/12 text-white">{service.title}</h2>
-              <div className="flex items-center gap-3">
+              <h2 className="text-[28px]/9 tracking-[1.4px] font-medium sm:text-[40px]/12 text-white text-center">{service.title}</h2>
+              <div className="flex items-center gap-4">
                 <span className="text-white uppercase text-[15px] tracking-[5px]">
                   View Project{" "}
                 </span>
-                <MdOutlineKeyboardArrowRight className="text-xl text-Peach" />
+              <Image src={RightArrow} alt="" />
               </div>
             </div>
           </Link>
